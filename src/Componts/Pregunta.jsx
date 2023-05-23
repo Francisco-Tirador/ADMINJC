3841d5ae15bbd4cf070caaf15c682f53e8f487ec
FraBeltran <jcfranciscoBeltran@gmail.com>
2023-05-23 16:21:49 -0600

    multiples servidores

--- src/Componts/Pregunta.jsx
+++ src/Componts/Pregunta.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import axios from 'axios'
 
 
-const Pregunta = ({ID,message,autor,refresh}) => {
+const Pregunta = ({ID,message,autor,refresh,api,color}) => {
 
-  let array=["no se escucha","pesimo","pesima","mal","mala"]
+  let array=["no se escucha","pesimo","pesima","mal","mala","bajito","no se entiende","problemas","no puedo entrar", "no abre","perdio","pierde","no puedo ver","no puedo entrar","fallando",
+"no puedo","no se ve","no hay sonido","no tengo audio","perdió la señal"
+]
   let asd=true
 
 const validacionContenido=(messsage)=>{
@@ -19,8 +21,9 @@ const validacionContenido=(messsage)=>{
 
 
 const removes=()=>{
-  let url=`https://jc-innovation.com/ad/ModelApi.php?removeById=${ID}`
-  axios.get(url,"sad")
+  console.log(api)
+  let url=`${api}${ID}`
+  axios.get(url)
   .then(api=>{
     console.log(api)
     refresh()
@@ -40,7 +43,9 @@ validacionContenido(message)
     <tr className={!asd?`warning`:'check'}>
     <th className='ContenPregunta'>{message}</th>
       <th className='ContenPregunta'>{autor}</th>
-      <button onClick={removes} >ELIMINAR</button>
+   
+     <button onClick={removes} >ELIMINAR</button>
+   
     </tr>
      
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,51 +6,95 @@ import { useForm } from 'react-hook-form'
 
 const Home = () => {
 
-const [ComentariosDB,setComentariosDB]=useState("")
-const [NumSesion,setNumSesion]=useState("")
+//! aqui se almacenan temporarlmente las rutas
+
+const apis=[
+  {name:"Amce",getAll:"https://jc-innovation.com/ad/ModelApi.php?GetById=",remove:"https://jc-innovation.com/ad/ModelApi.php?removeById="},
+  {name:"Ammr",getAll:"https://jc-innovation.com/ad/ammr/ModelApi.php?GetById=",remove:"https://jc-innovation.com/ad/ammr/ModelApi.php?removeById="},
+  {name:"AMMTAC",getAll:"https://jc-innovation.com/ad/ammtac/ModelApi.php?GetById=",remove:"https://jc-innovation.com/ad/ammtac/ModelApi.php?removeById="},
+  {name:"SOCMECIRPED",getAll:"https://jc-innovation.com/ad/SOCMECIRPED/ModelApi.php?GetById=",remove:"https://jc-innovation.com/ad/SOCMECIRPED/ModelApi.php?removeById="},
+  {name:"Comexane",getAll:"",remove:""},
+  {name:"Ameh",getAll:"",remove:""},
+] 
+//!
 
 
+const [ComentariosDB,setComentariosDB]=useState("")//!Aqui se almacena la informacion
+const [NumSesion,setNumSesion]=useState("")//! aqui se almacena el numero de la sesión
+const [User,setUser]=useState(0)//! aqui se almacena el numero de la sesión
+
+// const asd=
+
+//!aqui se manda a llamar la api
 const  GetAllpreguntas= ()=>{
-    let url= `https://jc-innovation.com/ad/ModelApi.php?GetById=${NumSesion}`
+    let url= `${apis[User].getAll}${NumSesion}`
      axios.get(url)
      .then(api=>{
-      console.log(api),
-      setComentariosDB(api.data.Comentarios),
-      console.log(ComentariosDB)
+      setComentariosDB(api.data.Comentarios)
     }
         )
-    .catch(err=>{console.log(err)
-      setComentariosDB(404)}
+    .catch(err=>{console.log(err)}
     )
+ 
   }
 
+
+  //! el usseefeect que se mantiene escuchando al numero de la sesion
   useEffect(() => {
     GetAllpreguntas()
   }, [NumSesion])
 
+// const bucle=()=>{
+//   GetAllpreguntas()
+// }
+
+
   const {handleSubmit,register}=useForm()  
 
+
+  //!aqui creamos se guarda la informacion del form
   const  submit= data=>{
       console.log(data)
       setNumSesion(data?.IDsesion)
+        //  GetAllpreguntas()
       console.log(NumSesion)
   }
 
 
-  const removeComentario=(ad)=>{
-    console.log("hola me removere"+ad)
+  const INDEX=[]
+  const RetornarIndex=apis.map(tar=>{INDEX.push(tar.name)})
+  
+
+  const valueUsuario=(valu)=>{
+    let valor=valu.target.value
+   let art= INDEX.indexOf(valor)
+     setUser(art)
+    console.log(valu)
   }
+
+
+
+
+// console.log(INDEX)
 
   return (
     <div >
+      <select name="Amce" id="Acme" onChange={valueUsuario}>
+     {
+      INDEX.map(Option=>(
+        <option key={Option} value={Option}>{Option}</option>
+      ))
+     }
+      </select>
+
       <form onSubmit={handleSubmit(submit)}>
             <label htmlFor="name"></label>
             <input  placeholder="Ingresa ID de la sesion" type="text" id='name' required  autoComplete='off'{...register("IDsesion")}/>
             <button >BUSCAR PREGUNTAS</button>
         </form>
-      <h3>PREGUNTAS DE AMCE MODULO {NumSesion}</h3>
+      <h3>PREGUNTAS DE {apis[User].name} MODULO   { NumSesion}</h3>
    <div className='Conten'>
-   <tr>
+
    {
           ComentariosDB?.[0]?
             ComentariosDB.map(Comentario=>(
@@ -60,8 +104,9 @@ const  GetAllpreguntas= ()=>{
                 message={Comentario.comentario}
                 autor={Comentario.nombre}
                 key={Comentario?.id}
-                remove={removeComentario}
                 refresh={GetAllpreguntas}
+                api={apis[User]?.remove}
+                color={apis[User]?.name}
                 />
               
               
@@ -69,7 +114,7 @@ const  GetAllpreguntas= ()=>{
             <div className='contenidoInicio'><h2>INGRESA UNA ID DE SESION PARA VISUALIZAR LAS PREGUNTAS</h2></div>
 }
 
-</tr>
+
 
    </div>
     </div>
